4ecd684be9f63904df02fb6b559a7cb698046186
eng wafaa <wafaajame@gmail.com>
2019-10-01 14:47:18 +0200

    committttttt

--- app/src/main/java/com/Tamazj/TamazjApp/Adapter/CategoriesAdapter.java
+++ app/src/main/java/com/Tamazj/TamazjApp/Adapter/CategoriesAdapter.java
@@ -44,6 +44,7 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.My
     }
 
     boolean isClickable;
+    
 
     @Override
     public void onBindViewHolder(@NonNull final CategoriesAdapter.MyHolder holder, final int position) {
